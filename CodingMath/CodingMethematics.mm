ba763e3d194eace61f2710abfa0e73c6b07e6b36
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-03-12 14:56:23 -0400

    initializing

---
+++ CodingMath/CodingMethematics.mm
@@ -0,0 +1,153 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="编程数学&#xa;Coding Methematics" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1710268553473">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#fdf6e3" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_solarized_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1704360557" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1704360557" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1083719472" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1083719472"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#fdf6e3">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="1. Octave的安装和使用" POSITION="bottom_or_right" ID="ID_623504470" CREATED="1710268577135" MODIFIED="1710268586113">
+<node TEXT="1.1 Octave的下载" ID="ID_1375133305" CREATED="1710268632504" MODIFIED="1710268637785"/>
+<node TEXT="1.2 Octave的安装" ID="ID_581994124" CREATED="1710268638217" MODIFIED="1710268644349"/>
+</node>
+<node TEXT="2. 消防所选址问题" POSITION="bottom_or_right" ID="ID_1329446901" CREATED="1710268586295" MODIFIED="1710268595750">
+<node TEXT="2.1 出租车距离的定义和例题" FOLDED="true" ID="ID_33017483" CREATED="1710268647784" MODIFIED="1710268661768">
+<node TEXT="2.1.1 什么是出租车距离" ID="ID_420296917" CREATED="1710268866758" MODIFIED="1710268879167"/>
+<node TEXT="2.1.2 出租车距离的计算方法" ID="ID_713684182" CREATED="1710268879377" MODIFIED="1710268890167"/>
+<node TEXT="2.1.3 计算出租车距离" ID="ID_12216428" CREATED="1710268890329" MODIFIED="1710268898637"/>
+<node TEXT="2.1.4 寻找X的最佳位置" ID="ID_716775956" CREATED="1710268898803" MODIFIED="1710268907870"/>
+<node TEXT="2.1.5 引用坐标概念计算出租车距离" ID="ID_86909461" CREATED="1710268911764" MODIFIED="1710268925287"/>
+<node TEXT="2.1.6 利用坐标计算出租车距离" ID="ID_1608937403" CREATED="1710268925498" MODIFIED="1710268937670"/>
+<node TEXT="2.1.7 将坐标一般化计算出租车距离" ID="ID_1955872643" CREATED="1710268937843" MODIFIED="1710268954614"/>
+<node TEXT="2.1.8 目的地数量众多时的出租车距离计算" ID="ID_605869018" CREATED="1710268955035" MODIFIED="1710268972349"/>
+<node TEXT="2.1.9 有没有更简单的方法" ID="ID_517616186" CREATED="1710268972514" MODIFIED="1710268982487"/>
+</node>
+<node TEXT="2.2 用Octave寻找消防所的最佳位置" FOLDED="true" ID="ID_787464526" CREATED="1710268662080" MODIFIED="1710268679374">
+<node TEXT="2.2.1 本章中使用的Octave的语句" ID="ID_1339613255" CREATED="1710268985805" MODIFIED="1710269079078"/>
+<node TEXT="2.2.2 随机点与特定点的出租车距离计算" ID="ID_814656179" CREATED="1710269001499" MODIFIED="1710269074749"/>
+<node TEXT="2.2.3 寻找随机向量中的最小值" ID="ID_821655228" CREATED="1710269018046" MODIFIED="1710269070334"/>
+<node TEXT="2.2.4 寻找随机矩阵中的最小值" ID="ID_448044435" CREATED="1710269031476" MODIFIED="1710269066676"/>
+<node TEXT="2.2.5 寻找消防所的最佳位置" ID="ID_1601966956" CREATED="1710269041173" MODIFIED="1710269057166"/>
+</node>
+</node>
+<node TEXT="3. 旅行商问题" POSITION="bottom_or_right" ID="ID_139222882" CREATED="1710268595913" MODIFIED="1710268604109">
+<node TEXT="3.1 什么是旅行商问题" ID="ID_1688610845" CREATED="1710268682376" MODIFIED="1710268690143"/>
+<node TEXT="3.2 用Octave找出快递的最短配送路径" FOLDED="true" ID="ID_1797945118" CREATED="1710268690354" MODIFIED="1710268704853">
+<node TEXT="3.2.1 本章中使用的Octave的语句" ID="ID_1074697211" CREATED="1710269093214" MODIFIED="1710269106422"/>
+<node TEXT="3.2.2 寻找快速配送路径" ID="ID_982120525" CREATED="1710269106786" MODIFIED="1710269118534"/>
+<node TEXT="3.2.3 最近处邻居算法" ID="ID_117607147" CREATED="1710269118675" MODIFIED="1710269129909"/>
+<node TEXT="3.2.4 利用遗传算法寻找最优路径1" ID="ID_1298699104" CREATED="1710269130066" MODIFIED="1710269157233"/>
+<node TEXT="3.2.5 利用遗传算法寻找最优路径2" ID="ID_488823846" CREATED="1710269157653" MODIFIED="1710269160407"/>
+</node>
+</node>
+<node TEXT="4. 蒙特卡罗模拟法" POSITION="bottom_or_right" ID="ID_1163030684" CREATED="1710268604259" MODIFIED="1710268614752">
+<node TEXT="4.1 概率：投掷硬币、掷骰(tou2)子" FOLDED="true" ID="ID_1772453906" CREATED="1710268708713" MODIFIED="1710268763934">
+<node TEXT="4.1.1 投掷硬币" ID="ID_223961029" CREATED="1710269166019" MODIFIED="1710269176773"/>
+<node TEXT="4.1.2 掷骰子" ID="ID_1240145071" CREATED="1710269176989" MODIFIED="1710269182244"/>
+</node>
+<node TEXT="4.2 用Octave实现蒙特卡罗模拟" FOLDED="true" ID="ID_1283553655" CREATED="1710268755292" MODIFIED="1710269321620" VGAP_QUANTITY="3 pt">
+<node TEXT="4.2.1 蒙特卡罗模拟法" ID="ID_342348685" CREATED="1710269183525" MODIFIED="1710269192558"/>
+<node TEXT="4.2.2 计算机骰子制作" ID="ID_986405051" CREATED="1710269192798" MODIFIED="1710269201144"/>
+<node TEXT="4.2.3 飞镖游戏" ID="ID_1089663964" CREATED="1710269203929" MODIFIED="1710269209788"/>
+<node TEXT="4.2.4 图形重叠区域面积的求解" ID="ID_1573642924" CREATED="1710269209966" MODIFIED="1710269227558"/>
+<node TEXT="4.2.5 随机活动的国际象棋棋子的位置查找" ID="ID_149236998" CREATED="1710269227731" MODIFIED="1710269243254"/>
+<node TEXT="4.2.6 一维线段上任意两点之间的距离问题" ID="ID_56781848" CREATED="1710269243429" MODIFIED="1710269321619"/>
+<node TEXT="4.2.7 两点间距离的概率分布情况" ID="ID_961739620" CREATED="1710269263406" MODIFIED="1710269277678"/>
+<node TEXT="4.2.8 二维空间中两点间的距离问题" ID="ID_672895713" CREATED="1710269277837" MODIFIED="1710269289233"/>
+<node TEXT="4.2.9 两点间距离的概率发布情况" ID="ID_512436252" CREATED="1710269289393" MODIFIED="1710269305216"/>
+<node TEXT="4.2.10 三维空间中两点间的距离问题" ID="ID_410138054" CREATED="1710269308319" MODIFIED="1710269317959"/>
+</node>
+</node>
+<node TEXT="5. 分形构造问题" POSITION="bottom_or_right" ID="ID_826794014" CREATED="1710268614885" MODIFIED="1710268624737">
+<node TEXT="5.1 何谓分形" ID="ID_1334469267" CREATED="1710268783666" MODIFIED="1710268791314"/>
+<node TEXT="5.2 运用Octave以编码实现分形构造" FOLDED="true" ID="ID_154751699" CREATED="1710268791619" MODIFIED="1710268813117">
+<node TEXT="5.2.1 本章中使用的Octave的语句" POSITION="bottom_or_right" ID="ID_1464514463" CREATED="1710269323973" MODIFIED="1710269341990"/>
+<node TEXT="5.2.2 旋转矩阵" POSITION="bottom_or_right" ID="ID_1172306813" CREATED="1710269342349" MODIFIED="1710269347384"/>
+<node TEXT="5.2.3 递归函数" POSITION="bottom_or_right" ID="ID_1627585395" CREATED="1710269350438" MODIFIED="1710269354247"/>
+<node TEXT="5.2.4 三角形的旋转" POSITION="bottom_or_right" ID="ID_746583948" CREATED="1710269354405" MODIFIED="1710269359816"/>
+<node TEXT="5.2.5 四边形的旋转" POSITION="bottom_or_right" ID="ID_1828208498" CREATED="1710269359981" MODIFIED="1710269366488"/>
+<node TEXT="5.2.6 六边形的旋转" POSITION="bottom_or_right" ID="ID_895530682" CREATED="1710269366652" MODIFIED="1710269372839"/>
+<node TEXT="5.2.7 分形树" POSITION="bottom_or_right" ID="ID_1976150628" CREATED="1710269372990" MODIFIED="1710269378319"/>
+<node TEXT="5.2.8 直角三角形的相似比" POSITION="bottom_or_right" ID="ID_725474841" CREATED="1710269378490" MODIFIED="1710269396120"/>
+<node TEXT="5.2.9 等比数列以及等比数列的和" POSITION="bottom_or_right" ID="ID_1657808369" CREATED="1710269390229" MODIFIED="1710269414663"/>
+<node TEXT="5.2.10 毕达哥拉斯树" POSITION="bottom_or_right" ID="ID_1079943453" CREATED="1710269417655" MODIFIED="1710269426272"/>
+</node>
+</node>
+</node>
+</map>
--- CodingMath/README.md
+++ CodingMath/README.md
@@ -10,3 +10,12 @@ The book can be purchased at jd.com: https://item.jd.com/12724025.html, or read 
 
 ## Book Structure 内容结构
 
+![L2-Mindmap](img/CodingMethematics-MindMap.png)
+
+## Step by Step Learning Video
+
+[Link in My YouTube Channel](https://www.youtube.com/playlist?list=PL6DEHvciXKeW7wVsceucyZTEK6-_CjPFq), speaking in Chinese Mandarin
+
+---
+
+Welcome to leave comments to my videos or drop me [email here](mailto:xiaoqizhao@outlook.com). And, thanks for supporting with your sponsor!
